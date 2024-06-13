ff91d533aa2f5955bfc38fa5110c798d4a8f7591
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2024-06-13 06:27:44 +0200

    [!/] Upgraded class interface..
    Previously it was bare struct and users have to allocate and release
    memory for list nodes by itself.
    
    Now Add() method accepts data, not pointer to allocated node. Memory
    is allocated in Add() and released in Release().
    
    Probably I'll write less-totalistic Remove() to remove first element
    and release it's memory..
    
    omfg i've already declared this Remove() in header and GNU C++ gave
    zero warnings, compiled and uploaded code with unimplemented interface.
    actually trash language

--- examples/me_List/me_List.ino
+++ examples/me_List/me_List.ino
@@ -2,7 +2,7 @@
 
 /*
   Author: Martin Eden
-  Last mod.: 2024-06-11
+  Last mod.: 2024-06-13
 */
 
 #include <me_List.h>
@@ -34,37 +34,65 @@ using
   me_BaseTypes::TBool,
   me_BaseTypes::TUint_2;
 
+// Forwards:
+TBool PrintNode(TUint_2 NodeData, TUint_2 HandlerData);
+
+// Show stuff
 void Test()
 {
-  using
-    me_List::TStack,
-    me_List::TQueue,
-    me_List::TListNode;
+  /*
+    We will create stack (or maybe queue) list, add some numbers to it,
+    print it and release memory.
 
-  TStack List;
-  // TQueue List;
+    Actually memory will be released when <List> variable leave scope
+    i.e. at end of function. So List.Release() here is not strictly
+    needed.
+
+    * Memory leaks test
+
+      You can test that memory is properly released by trying to
+      allocate more nodes that can be fit into memory and calling this
+      function several times: "Counter <= 4000, Test(); Test();"
+
+      Number of nodes allocated and printed should stay the same.
+
+    * No memory condition
+
+      When we have no memory to allocate next node,
+      Add() will return false.
+  */
+
+  // me_List::TStack List;
+  me_List::TQueue List;
 
   for (TUint_2 Counter = 1; Counter <= 4; ++Counter)
-  {
-    TListNode * Node;
+    List.Add(Counter);
 
-    if (!SpawnNode(&Node))
-      break;
+  List.Traverse(PrintNode);
 
-    Node->Data = Counter;
-
-    List.Add(Node);
-  }
-
-  Traverse(List.Head, PrintNode);
-
-  KillList(List.Head);
+  List.Release();
 }
 
-me_BaseTypes::TBool PrintNode(me_BaseTypes::TUint_2 Data)
+/*
+  Node handler
+
+  For egocentric functions you need just your data.
+
+  For cases like "find node with minimum value" you want to store
+  your findings somewhere. Or load additional data from somewhere,
+  if you are doing "find node with this value". That's why second
+  address, <HandlerData>. Put there address of your object and you
+  can call it's methods.
+
+  When handler returns false it means iteration over list should
+  be stopped.
+*/
+TBool PrintNode(
+  TUint_2 NodeData,
+  TUint_2 HandlerData __attribute__((unused))
+)
 {
-  printf("Data(%u)", Data);
-  printf("\n");
+  printf("Data(%u)\n", NodeData);
 
   return true;
 }
@@ -73,4 +101,5 @@ me_BaseTypes::TBool PrintNode(me_BaseTypes::TUint_2 Data)
   2024-05-14
   2024-06-02
   2024-06-11
+  2024-06-13
 */
